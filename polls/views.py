a0ce1a134bc09bc280948e7e068a109d4f8e6588
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2023-03-02 14:37:18 +0100

    Update the view -vote- so it handles the submitted data (of detail.html) and does something with it.

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 # from django.template import loader
 from .models import Question
 
@@ -21,4 +21,19 @@ def results (request, question_id) :
     return HttpResponse (response % question_id)
 
 def vote (request, question_id) :
-    return HttpResponse("Your're voting on question %s" % question_id)
+    question = get_object_or_404 (Question, pk = question_id)
+    try :
+        selected_choice = question.choise_set.get (pk = request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist) :
+        # Redisplay the question voting form
+        return render (request, 'polls/detail.html', {
+            'question' : question,
+            'error_message' : "You didn't select a choice.",
+        })
+    else :
+        selected_choice.votes += 1
+        selected_choice.save ()
+        # Always return a HttpResponseRedirect  after successfully dealing
+        # with POST data. This prevents data from beung posted twice if a
+        # user hits Back button.
+        return HttpResponseRedirect (reverse ("polls/results", args (question_id)))
